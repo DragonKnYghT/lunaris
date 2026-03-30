6e6861dc01221d8ea2895702a2946707d83f38f7
DragonKnYghT <rodrigoduarteferreira2009@gmail.com>
2026-03-30 23:29:26 +0200

    1.7.18

--- main.js
+++ main.js
@@ -680,7 +680,7 @@ function startGame(mode, playerCount, isMultiplayer) {
                     { name: 'Griffe', power: 10, pp: 35, maxPp: 35, type: 'Normal' },
                     { name: 'Rugissement', power: 0, pp: 40, maxPp: 40, type: 'Normal' }
                 ],
-                sprite: 'assets/sprite/lunaris1.png'
+                sprite: 'sprite/Lunaris/Starteur/Starteur 1.png'
             },
             enemy: {
                 name: 'Lunaris 2',
@@ -691,7 +691,7 @@ function startGame(mode, playerCount, isMultiplayer) {
                 moves: [
                     { name: 'Charge', power: 8, pp: 35, maxPp: 35 }
                 ],
-                sprite: 'assets/sprite/lunaris2.png'
+                sprite: 'sprite/Lunaris/Starteur/Starteur 2.png'
             },
             isPlayerTurn: true
         };
